c62f71dded33d6d93cafa2869ba31bf5244941fd
xluo12 <33305052+xluo12@users.noreply.github.com>
2019-03-24 10:11:10 -0400

    Update format

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import time
 import smtplib
 
+
 def send_alert(message):
     gmail_user = 'xxx@xxx'    # your email address
     gmail_password = 'xxxxxx'   # your email password
@@ -25,11 +26,12 @@ def send_alert(message):
     server.login(gmail_user, gmail_password)
     server.sendmail(sent_from, to, email_text)
     server.close()
-
     print('Email alert sent!')
 
+    
 browser = webdriver.Chrome()
 browser.implicitly_wait(5)
+
 browser.get('http://albert.nyu.edu/albert_index.html?FolderPath=PORTAL_ROOT_OBJECT.NYU_STUDENT_CTR&IsFolder=false&IgnoreParamTempl=FolderPath,IsFolder')
 browser.find_element_by_class_name('buttonLink').click()
 browser.find_element_by_id('netid').send_keys('xxxxx')   # your NetID
@@ -41,10 +43,12 @@ browser.find_element_by_id('passcode').click()
 browser.find_element_by_xpath('//input[@name="passcode"]').send_keys('xxxxxx')  # your passcode received on your cellphone
 browser.find_element_by_id('passcode').click()
 browser.find_element_by_xpath('//a[@class="nyuCourseSearch"]').click()
+
 while len(browser.find_elements_by_xpath('//table[@class="PSLEVEL1GRID accordion-table"]')) == 0:
     browser.find_element_by_xpath('//a[@class="nyuCourseSearch"]').click()
 to_quit = False
 message = ''
+
 while True:
     # modify below format according to your courses layout in your shopping cart
     if browser.find_element_by_xpath('//table[@class="PSLEVEL1GRID accordion-table"]/tbody[1]/tr[2]/td[7]/div[1]/div[1]/img[1]').get_attribute('src') == 'https://sis.nyu.edu/cs/csprod/cache/855/PS_CS_STATUS_OPEN_ICN_1.gif':
